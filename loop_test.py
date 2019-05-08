a103eab9ad17048efa955571afced92199914b3f
Todd <mrtsart@gmail.com>
2019-05-08 18:23:48 -0500

    Update loop_test.py
    Loop Works

--- loop_test.py
+++ loop_test.py
@@ -1,8 +1,9 @@
 # Faab Reducer FUnction Test
 
-x = [5,4,6,7,4,5]
-faab = 4
+x = [5, 4, 6, 7, 40, 5]
+faab = 40
 print(len(x))
+
 
 def test():
     global faab
@@ -14,15 +15,20 @@ def test():
                                             # Goal is to see if all $1
             while x[i] == 1:            # when a salary = 1 skip
                 i = i + 1
+                continue
 
             else:
-                x[i] = x[i] - 1         # reduce Salary
-                faab = faab -1          # reduce faab
-                i = i + 1               # next entry
-                print("my faab" + str(faab))
-                print(x)
-                if i >= ( len(x) - 1):  # when we reach the last entry
-                    i = 0               # reset to 0
+                if faab > 0:
+                    x[i] = x[i] - 1         # reduce Salary
+                    faab = faab - 1          # reduce faab
+                    i = i + 1               # next entry
+                    print("my faab" + str(faab))
+                    print(x)
+                    if i >= (len(x) - 1):  # when we reach the last entry
+                        i = 0
+                        continue
+                    continue            # reset to 0
+                break
 
         else:
             print("Salaries are all $1")
@@ -30,7 +36,6 @@ def test():
 
     else:
         print("Out of Faab")
-
 
 
 test()
